993b105adff1328a7f05c8ceaab1db3d2bd4324f
Miikka Koskinen <miikka@metosin.fi>
2019-07-19 08:47:50 +0300

    Update Codecov reporter to use a new report format
    The new format allows us include the form coverage information in
    Codecov as well.

--- cloverage/src/cloverage/report/codecov.clj
+++ cloverage/src/cloverage/report/codecov.clj
@@ -5,20 +5,21 @@
    [cloverage.report :refer [line-stats with-out-writer]]))
 
 (defn- file-coverage [[file file-forms]]
-  ;; https://codecov.io/api#post-json-report
-  ;; > 0: covered (number of times hit)
-  ;; true: partially covered
+  ;; https://docs.codecov.io/reference#section-codecov-json-report-format
   ;; 0: not covered
+  ;; "1/3": partially covered, 1 form out of 3 covered
+  ;; > 0: covered (number of times hit)
   ;; null: skipped/ignored/empty
-  ;; the first item in the list must be a null
   (vector file
-          (cons nil
-                (mapv (fn [line]
+          (into {}
+                (map (fn [line]
+                       [(str (:line line))
                         (cond (:blank?   line) nil
                               (:covered? line) (:times-hit line)
-                              (:partial? line) true
+                              (:partial? line) (str (:hit line) "/" (:total line))
                               (:instrumented? line) 0
-                              :else nil)) (line-stats file-forms)))))
+                              :else nil)]))
+                (line-stats file-forms)) ))
 
 (defn report [^String out-dir forms]
   (let [output-file (io/file out-dir "codecov.json")
